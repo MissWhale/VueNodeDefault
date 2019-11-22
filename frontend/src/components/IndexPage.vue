52d16c7c40ed3e40ebee38617b110c392b7b6300
MissWhale <doden5180@gmail.com>
2019-11-22 09:57:57 +0900

    초기설정

---
+++ frontend/src/components/IndexPage.vue
@@ -0,0 +1,53 @@
+<template lang="jade">
+.movies
+        h1 영화 목록
+        .movie(v-for="movie in movies", v-bind:key="movie.id")
+                img.poster(v-bind:src="movie.poster")
+                div
+                        strong {{movie.name}}
+                        |  , 
+                        i [{{movie.director}}]
+                        |  {{movie.year}}
+                        router-link(:to="{ name: 'show', params: { id: movie.id }}") 더보기
+//-   <div class="movies">
+//-     <h1>영화 목록</h1>
+//-     <div v-for="movie in movies" v-bind:key="movie.id" class="movie">
+//-       <img v-bind:src="movie.poster" class="poster">
+//-       <div>
+//-         <strong>{{movie.name}}</strong>, <i>{{movie.director}}</i> [{{movie.year}}]
+//-         <router-link :to="{ name: 'show', params: { id: movie.id }}">더보기</router-link>
+//-       </div>
+//-     </div>
+//-   </div>
+</template>
+
+<script>
+export default {
+  created () {
+    this.$http.get('/api/movies')
+    .then((response) => {
+      this.movies = response.data
+    })
+  },
+//   name: 'hello',
+  data () {
+    return {
+      movies: []
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.poster {
+  width: 185px;
+  vertical-align: middle;
+}
+
+.movie {
+  display: inline-block;
+  padding: 10px;
+  text-align: center;
+}
+</style>
---
+++ frontend/src/components/ShowPage.vue
@@ -0,0 +1,30 @@
+<template lang="jade">
+div
+        h1 상세내용
+        p {{movie}}
+//-    <div>
+//-      <h1>상세 내용</h1>
+//-      {{movie}}
+//-    </div>
+</template>
+
+<script>
+export default {
+  created: function () {
+    var id = this.$route.params.id
+    this.$http.get(`/api/movies/${id}`)
+    .then((response) => {
+      this.movie = response.data
+    })
+  },
+  data: function () {
+    return {
+      movie: {}
+    }
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
